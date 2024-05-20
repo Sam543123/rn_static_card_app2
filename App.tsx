eedf29a04a80686b5df67dfa282a53a0d725e580
Semyon Sazanchuk <semyon.chuk@gmail.com>
2024-05-20 12:47:14 +0300

    Add basis of app2.

--- App.tsx
+++ App.tsx
@@ -1,36 +1,23 @@
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import FlatCards from './components/FlatCards'
+import ElevatedCards from './components/ElevatedCards'
+// import FancyCard from './components/FancyCard'
+// import ActionCard from './components/ActionCard'
+// import ContactList from './components/ContactList'
 
-import {
-    View,
-    Text,
-    StyleSheet,
-    useColorScheme
-} from 'react-native'
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  
-  return(
-      <View style={styles.container}>
-          <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
-              Hello World !
-          </Text>
-      </View>
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+        <ElevatedCards/>
+        {/* <FancyCard />
+        <ContactList/>
+        <ActionCard /> */}
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
-  },
-  whiteText: {
-      color: '#FFFFFF'
-  },
-  darkText: {
-      color: '#000000'
-  }
-})
-
-export default App;
+export default App
